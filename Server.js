a249565b2322a35b5ff469e86ce941f09e775af5
leotyps <113692736+leotyps@users.noreply.github.com>
2025-06-23 22:40:08 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const blessed = require('blessed');
 const cors = require('cors');
 
 const configPath = path.join(__dirname, 'config.json');
-let config = { IPWhitelist: false, WhitelistedIPs: [], Dashboard: true, Port: 3000, UseGithubMutationData: true };
+let config = { IPWhitelist: true, WhitelistedIPs: ["114.10.121.97", "20.9.130.252"], Dashboard: true, Port: 3000, UseGithubMutationData: true };
 
 if (fs.existsSync(configPath)) {
   config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
